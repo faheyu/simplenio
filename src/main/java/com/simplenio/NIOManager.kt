e7a20aed652daca035ab01b82f142161e1815b9a
faheyu11 <hieuqhz111@gmail.com>
2024-07-21 21:01:10 +0700

    add docs; fix connection can not queue

--- src/main/java/com/simplenio/NIOManager.kt
+++ src/main/java/com/simplenio/NIOManager.kt
@@ -28,6 +28,11 @@ object NIOManager {
         }
     }
 
+    /**
+     * close and remove channel from channel-selector map
+     *
+     * @param ioChannel channel to close
+     */
     fun closeChannel(ioChannel: IOChannel) {
         getSelectorThread(ioChannel).closeChannel(ioChannel)
         // remove from map
@@ -36,6 +41,12 @@ object NIOManager {
         }
     }
 
+    /**
+     * request a connection, wait until connection queue is not full
+     *
+     * @param ioChannel channel perform connection
+     * @param onConnect callback invoked when start connection
+     */
     fun pendingConnect(ioChannel: IOChannel, onConnect: suspend () -> Unit) {
         connectExecutor.launchCoroutine {
             waitForPending(ioChannel)
@@ -47,10 +58,19 @@ object NIOManager {
         }
     }
 
+    /**
+     * register a channel to a selector thread
+     */
     fun register(ioChannel: IOChannel, ops: Int) {
         getSelectorThread(ioChannel).register(ioChannel, ops)
     }
 
+
+    /**
+     * get selector thread has minimum registered channels
+     *
+     * @param ioChannel channel need to register
+     */
     private fun getSelectorThread(ioChannel: IOChannel) : SelectorThread {
         synchronized(channelSelectorMap) {
             var selectorThread = channelSelectorMap[ioChannel]
@@ -64,10 +84,13 @@ object NIOManager {
                 }
             }
 
-            selectorThread = channelSelectorMap.values.minByOrNull {
-                it.channelCount
-            } ?: SelectorThread()
+            selectorThread = channelSelectorMap.values
+                .minByOrNull {
+                    it.channelCount
+                }
+                ?: SelectorThread() // create new one if list is empty
 
+            // if this selector thread reach the limit, create new one
             if (selectorThread.channelCount > CHANNEL_PER_SELECTOR) {
                 selectorThread = SelectorThread()
             }
@@ -78,19 +101,38 @@ object NIOManager {
         }
     }
 
+    /**
+     * wait until connection queue is not full
+     *
+     * @param ioChannel the channel add to the queue
+     */
     private suspend fun waitForPending(ioChannel: IOChannel) {
+        // try to add this channel to queue
         while (!pendingConnections.offer(ioChannel)) {
             logger.d("waiting for pending: ${pendingConnections.size}")
+
+            // suspend here until resumed (maybe by selector thread)
             suspendCoroutine {
+                // add ref to list
+                // so we can retrieve and resume it
+                // after another connection is established
                 connectContinuations.add(it)
             }
         }
-        pendingConnections.remove()
     }
 
+    /**
+     * resume a pending connection (after a connection is established),
+     * make it continue to connect or queue again
+     * if the queue still full
+     */
     fun resumeConnect() {
         connectContinuations.poll()?.apply {
             resume(Unit)
+            // remove a channel from queue
+            // so another connection can be queued
+            // the queue should not be empty here
+            pendingConnections.remove()
         }
     }
 }
