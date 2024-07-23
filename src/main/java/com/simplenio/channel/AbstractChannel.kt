cbab944fd894576d746a60a88be6893b77179bd5
faheyu11 <hieuqhz111@gmail.com>
2024-07-23 21:19:57 +0700

    improve docs

--- src/main/java/com/simplenio/channel/AbstractChannel.kt
+++ src/main/java/com/simplenio/channel/AbstractChannel.kt
@@ -1,6 +1,7 @@
 package com.simplenio.channel
 
 import com.simplenio.IOHandler
+import com.simplenio.MyThreadPoolExecutor
 import com.simplenio.NIOManager
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
@@ -8,17 +9,22 @@ import java.nio.ByteBuffer
 abstract class AbstractChannel(val socketAddress: InetSocketAddress) : IOHandler {
 
     companion object {
+        /**
+         * handle background tasks
+         */
+        internal val threadPool = MyThreadPoolExecutor(1)
+
         const val READ_BUFFER_SIZE = 8192
         const val WRITE_BUFFER_SIZE = 8192
     }
 
     /**
-     * data buffer read from socket
+     * data buffer read from channel
      */
     protected val readBuffer = ByteBuffer.allocate(READ_BUFFER_SIZE)
 
     /**
-     * data buffer write to socket
+     * data buffer write to channel
      */
     protected val writeBuffer = ByteBuffer.allocate(WRITE_BUFFER_SIZE)
 
@@ -28,19 +34,21 @@ abstract class AbstractChannel(val socketAddress: InetSocketAddress) : IOHandler
     abstract fun connect(): Boolean
 
     /**
-     * write data to socket
+     * write data to buffer, this buffer will be write to channel in selector thread
+     *
+     * @return true if buffer's capacity > 0 else false
      */
     abstract fun send(byteBuffer: ByteBuffer): Boolean
 
     /**
-     * close current socket (maybe connected or not)
+     * close current channel (maybe connected or not)
      */
     open fun close() {
         NIOManager.closeChannel(this)
     }
 
     protected fun finalize() {
-        // somehow we forget to close socket
+        // somehow we forget to close channel
         // this is "memory guard"
         close()
     }
