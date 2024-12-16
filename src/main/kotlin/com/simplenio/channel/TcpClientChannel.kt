e07a7becc8cbe09d313d1ba8f1c47f72d98c6e1b
faheyu11 <hieuqhz111@gmail.com>
2024-12-16 22:01:14 +0700

    fix api level 22

--- src/main/kotlin/com/simplenio/channel/TcpClientChannel.kt
+++ src/main/kotlin/com/simplenio/channel/TcpClientChannel.kt
@@ -53,7 +53,7 @@ open class TcpClientChannel : AbstractChannel {
     private var lastActiveTime = 0L
 
     constructor(socketAddress: InetSocketAddress): super(socketAddress)
-    constructor(connectedSocket: SocketChannel) : super(connectedSocket.remoteAddress as InetSocketAddress) {
+    constructor(connectedSocket: SocketChannel) : super(connectedSocket.socket().remoteSocketAddress as InetSocketAddress) {
         if (!connectedSocket.isConnected)
             throw IllegalArgumentException("socket is not connected")
 
